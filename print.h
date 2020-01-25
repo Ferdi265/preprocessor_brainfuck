a3d60239b7a2c32b4962a9dbe842893a99bc0073
Ferdinand Bachmann <theferdi265@gmail.com>
2020-01-25 04:39:42 +0100

    better print.h

--- print.h
+++ print.h
@@ -1,2 +1,518 @@
-PRINT
+#if 0
+#elif (PRINT) == 0
+0x00
+#elif (PRINT) == 1
+0x01
+#elif (PRINT) == 2
+0x02
+#elif (PRINT) == 3
+0x03
+#elif (PRINT) == 4
+0x04
+#elif (PRINT) == 5
+0x05
+#elif (PRINT) == 6
+0x06
+#elif (PRINT) == 7
+'\a'
+#elif (PRINT) == 8
+'\b'
+#elif (PRINT) == 9
+'\t'
+#elif (PRINT) == 10
+'\n'
+#elif (PRINT) == 11
+'\v'
+#elif (PRINT) == 12
+'\f'
+#elif (PRINT) == 13
+'\r'
+#elif (PRINT) == 14
+0x0e
+#elif (PRINT) == 15
+0x0f
+#elif (PRINT) == 16
+0x10
+#elif (PRINT) == 17
+0x11
+#elif (PRINT) == 18
+0x12
+#elif (PRINT) == 19
+0x13
+#elif (PRINT) == 20
+0x14
+#elif (PRINT) == 21
+0x15
+#elif (PRINT) == 22
+0x16
+#elif (PRINT) == 23
+0x17
+#elif (PRINT) == 24
+0x18
+#elif (PRINT) == 25
+0x19
+#elif (PRINT) == 26
+0x1a
+#elif (PRINT) == 27
+0x1b
+#elif (PRINT) == 28
+0x1c
+#elif (PRINT) == 29
+0x1d
+#elif (PRINT) == 30
+0x1e
+#elif (PRINT) == 31
+0x1f
+#elif (PRINT) == 32
+ 
+#elif (PRINT) == 33
+!
+#elif (PRINT) == 34
+"
+#elif (PRINT) == 35
+'#'
+#elif (PRINT) == 36
+$
+#elif (PRINT) == 37
+%
+#elif (PRINT) == 38
+&
+#elif (PRINT) == 39
+'
+#elif (PRINT) == 40
+(
+#elif (PRINT) == 41
+)
+#elif (PRINT) == 42
+*
+#elif (PRINT) == 43
++
+#elif (PRINT) == 44
+,
+#elif (PRINT) == 45
+-
+#elif (PRINT) == 46
+.
+#elif (PRINT) == 47
+/
+#elif (PRINT) == 48
+0
+#elif (PRINT) == 49
+1
+#elif (PRINT) == 50
+2
+#elif (PRINT) == 51
+3
+#elif (PRINT) == 52
+4
+#elif (PRINT) == 53
+5
+#elif (PRINT) == 54
+6
+#elif (PRINT) == 55
+7
+#elif (PRINT) == 56
+8
+#elif (PRINT) == 57
+9
+#elif (PRINT) == 58
+:
+#elif (PRINT) == 59
+;
+#elif (PRINT) == 60
+<
+#elif (PRINT) == 61
+=
+#elif (PRINT) == 62
+>
+#elif (PRINT) == 63
+?
+#elif (PRINT) == 64
+@
+#elif (PRINT) == 65
+A
+#elif (PRINT) == 66
+B
+#elif (PRINT) == 67
+C
+#elif (PRINT) == 68
+D
+#elif (PRINT) == 69
+E
+#elif (PRINT) == 70
+F
+#elif (PRINT) == 71
+G
+#elif (PRINT) == 72
+H
+#elif (PRINT) == 73
+I
+#elif (PRINT) == 74
+J
+#elif (PRINT) == 75
+K
+#elif (PRINT) == 76
+L
+#elif (PRINT) == 77
+M
+#elif (PRINT) == 78
+N
+#elif (PRINT) == 79
+O
+#elif (PRINT) == 80
+P
+#elif (PRINT) == 81
+Q
+#elif (PRINT) == 82
+R
+#elif (PRINT) == 83
+S
+#elif (PRINT) == 84
+T
+#elif (PRINT) == 85
+U
+#elif (PRINT) == 86
+V
+#elif (PRINT) == 87
+W
+#elif (PRINT) == 88
+X
+#elif (PRINT) == 89
+Y
+#elif (PRINT) == 90
+Z
+#elif (PRINT) == 91
+[
+#elif (PRINT) == 92
+'\\'
+#elif (PRINT) == 93
+]
+#elif (PRINT) == 94
+^
+#elif (PRINT) == 95
+_
+#elif (PRINT) == 96
+`
+#elif (PRINT) == 97
+a
+#elif (PRINT) == 98
+b
+#elif (PRINT) == 99
+c
+#elif (PRINT) == 100
+d
+#elif (PRINT) == 101
+e
+#elif (PRINT) == 102
+f
+#elif (PRINT) == 103
+g
+#elif (PRINT) == 104
+h
+#elif (PRINT) == 105
+i
+#elif (PRINT) == 106
+j
+#elif (PRINT) == 107
+k
+#elif (PRINT) == 108
+l
+#elif (PRINT) == 109
+m
+#elif (PRINT) == 110
+n
+#elif (PRINT) == 111
+o
+#elif (PRINT) == 112
+p
+#elif (PRINT) == 113
+q
+#elif (PRINT) == 114
+r
+#elif (PRINT) == 115
+s
+#elif (PRINT) == 116
+t
+#elif (PRINT) == 117
+u
+#elif (PRINT) == 118
+v
+#elif (PRINT) == 119
+w
+#elif (PRINT) == 120
+x
+#elif (PRINT) == 121
+y
+#elif (PRINT) == 122
+z
+#elif (PRINT) == 123
+{
+#elif (PRINT) == 124
+|
+#elif (PRINT) == 125
+}
+#elif (PRINT) == 126
+~
+#elif (PRINT) == 127
+0x7f
+#elif (PRINT) == 128
+128
+#elif (PRINT) == 129
+129
+#elif (PRINT) == 130
+130
+#elif (PRINT) == 131
+131
+#elif (PRINT) == 132
+132
+#elif (PRINT) == 133
+133
+#elif (PRINT) == 134
+134
+#elif (PRINT) == 135
+135
+#elif (PRINT) == 136
+136
+#elif (PRINT) == 137
+137
+#elif (PRINT) == 138
+138
+#elif (PRINT) == 139
+139
+#elif (PRINT) == 140
+140
+#elif (PRINT) == 141
+141
+#elif (PRINT) == 142
+142
+#elif (PRINT) == 143
+143
+#elif (PRINT) == 144
+144
+#elif (PRINT) == 145
+145
+#elif (PRINT) == 146
+146
+#elif (PRINT) == 147
+147
+#elif (PRINT) == 148
+148
+#elif (PRINT) == 149
+149
+#elif (PRINT) == 150
+150
+#elif (PRINT) == 151
+151
+#elif (PRINT) == 152
+152
+#elif (PRINT) == 153
+153
+#elif (PRINT) == 154
+154
+#elif (PRINT) == 155
+155
+#elif (PRINT) == 156
+156
+#elif (PRINT) == 157
+157
+#elif (PRINT) == 158
+158
+#elif (PRINT) == 159
+159
+#elif (PRINT) == 160
+160
+#elif (PRINT) == 161
+161
+#elif (PRINT) == 162
+162
+#elif (PRINT) == 163
+163
+#elif (PRINT) == 164
+164
+#elif (PRINT) == 165
+165
+#elif (PRINT) == 166
+166
+#elif (PRINT) == 167
+167
+#elif (PRINT) == 168
+168
+#elif (PRINT) == 169
+169
+#elif (PRINT) == 170
+170
+#elif (PRINT) == 171
+171
+#elif (PRINT) == 172
+172
+#elif (PRINT) == 173
+173
+#elif (PRINT) == 174
+174
+#elif (PRINT) == 175
+175
+#elif (PRINT) == 176
+176
+#elif (PRINT) == 177
+177
+#elif (PRINT) == 178
+178
+#elif (PRINT) == 179
+179
+#elif (PRINT) == 180
+180
+#elif (PRINT) == 181
+181
+#elif (PRINT) == 182
+182
+#elif (PRINT) == 183
+183
+#elif (PRINT) == 184
+184
+#elif (PRINT) == 185
+185
+#elif (PRINT) == 186
+186
+#elif (PRINT) == 187
+187
+#elif (PRINT) == 188
+188
+#elif (PRINT) == 189
+189
+#elif (PRINT) == 190
+190
+#elif (PRINT) == 191
+191
+#elif (PRINT) == 192
+192
+#elif (PRINT) == 193
+193
+#elif (PRINT) == 194
+194
+#elif (PRINT) == 195
+195
+#elif (PRINT) == 196
+196
+#elif (PRINT) == 197
+197
+#elif (PRINT) == 198
+198
+#elif (PRINT) == 199
+199
+#elif (PRINT) == 200
+200
+#elif (PRINT) == 201
+201
+#elif (PRINT) == 202
+202
+#elif (PRINT) == 203
+203
+#elif (PRINT) == 204
+204
+#elif (PRINT) == 205
+205
+#elif (PRINT) == 206
+206
+#elif (PRINT) == 207
+207
+#elif (PRINT) == 208
+208
+#elif (PRINT) == 209
+209
+#elif (PRINT) == 210
+210
+#elif (PRINT) == 211
+211
+#elif (PRINT) == 212
+212
+#elif (PRINT) == 213
+213
+#elif (PRINT) == 214
+214
+#elif (PRINT) == 215
+215
+#elif (PRINT) == 216
+216
+#elif (PRINT) == 217
+217
+#elif (PRINT) == 218
+218
+#elif (PRINT) == 219
+219
+#elif (PRINT) == 220
+220
+#elif (PRINT) == 221
+221
+#elif (PRINT) == 222
+222
+#elif (PRINT) == 223
+223
+#elif (PRINT) == 224
+224
+#elif (PRINT) == 225
+225
+#elif (PRINT) == 226
+226
+#elif (PRINT) == 227
+227
+#elif (PRINT) == 228
+228
+#elif (PRINT) == 229
+229
+#elif (PRINT) == 230
+230
+#elif (PRINT) == 231
+231
+#elif (PRINT) == 232
+232
+#elif (PRINT) == 233
+233
+#elif (PRINT) == 234
+234
+#elif (PRINT) == 235
+235
+#elif (PRINT) == 236
+236
+#elif (PRINT) == 237
+237
+#elif (PRINT) == 238
+238
+#elif (PRINT) == 239
+239
+#elif (PRINT) == 240
+240
+#elif (PRINT) == 241
+241
+#elif (PRINT) == 242
+242
+#elif (PRINT) == 243
+243
+#elif (PRINT) == 244
+244
+#elif (PRINT) == 245
+245
+#elif (PRINT) == 246
+246
+#elif (PRINT) == 247
+247
+#elif (PRINT) == 248
+248
+#elif (PRINT) == 249
+249
+#elif (PRINT) == 250
+250
+#elif (PRINT) == 251
+251
+#elif (PRINT) == 252
+252
+#elif (PRINT) == 253
+253
+#elif (PRINT) == 254
+254
+#elif (PRINT) == 255
+255
+#else
+FATAL: PRINT is not a valid number
+#   error "invalid number"
+#endif
 #undef PRINT
