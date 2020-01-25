513782c3577def33da738829c7c6891586a26784
Ferdinand Bachmann <theferdi265@gmail.com>
2020-01-25 16:01:58 +0100

    add debug prints

--- bf_iter.h
+++ bf_iter.h
@@ -6,30 +6,39 @@
 #endif
 #
 #if STATE == EXECUTE
+execute: cell = T0, cell index = Tr
 #   if C0 == '>'
+opcode >
 #       include "shift_T.h"
 #   elif C0 == '<'
+opcode <
 #       include "rshift_T.h"
 #   elif C0 == '+'
+opcode +
 #       define T0_NEXT ((T0 + 1) & 0xff)
 #       include "literals/T0.h"
 #   elif C0 == '-'
+opcode -
 #       define T0_NEXT ((T0 - 1) & 0xff)
 #       include "literals/T0.h"
 #   elif C0 == '.'
+opcode .
 #       define PRINT T0
 #       include "print.h"
 #   elif C0 == ','
+opcode ,
 #       define T0_NEXT I0
 #       include "literals/T0.h"
 #       include "shift_I.h"
 #   elif C0 == '['
+opcode [
 #       if T0 == 0
 #           undef STATE
 #           define STATE SKIP
 #           define NESTCOUNT 0
 #       endif
 #   elif C0 == ']'
+opcode ]
 #       if T0 != 0
 #           undef STATE
 #           define STATE RSKIP
@@ -37,16 +46,20 @@
 #           include "rshift_C.h"
 #       endif
 #   elif C0 == 'X'
+opcode X
 #       define HALT 1
 #   endif
 #   if STATE == EXECUTE
 #       include "shift_C.h"
 #   endif
 #elif STATE == SKIP
+skip: nest = NESTCOUNT
 #   if C0 == '['
+skipcode [
 #       define NESTCOUNT_NEXT (NESTCOUNT + 1)
 #       include "literals/NESTCOUNT.h"
 #   elif C0 == ']'
+skipcode ]
 #       if NESTCOUNT == 0
 #           undef STATE
 #           define STATE EXECUTE
@@ -58,10 +71,13 @@
 #   endif
 #   include "shift_C.h"
 #elif STATE == RSKIP
+rskip: nest = NESTCOUNT
 #   if C0 == ']'
+rskipcode ]
 #       define NESTCOUNT_NEXT (NESTCOUNT + 1)
 #       include "literals/NESTCOUNT.h"
 #   elif C0 == '['
+rskipcode [
 #       if NESTCOUNT == 0
 #           undef STATE
 #           define STATE EXECUTE
